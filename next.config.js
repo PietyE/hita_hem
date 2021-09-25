b4ffa12f1f885ead47a2e89a644dbf69662ed620
Vovk Viktor <viktor.vovk@vilmate.com>
2021-09-25 13:48:44 +0300

    add cache-control

--- next.config.js
+++ next.config.js
@@ -10,4 +10,18 @@ module.exports = withBundleAnalyzer({
     autoPrerender: false,
     buildActivity: false,
   },
+  async headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|png)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 });
