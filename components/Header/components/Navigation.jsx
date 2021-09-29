53a38ca4528213e21450cbb9eaeb962f94dcf5aa
Vovk Viktor <viktor.vovk@vilmate.com>
2021-09-29 12:54:10 +0300

    fix

--- components/Header/components/Navigation.jsx
+++ components/Header/components/Navigation.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useRef, useState } from "react";
+import { useCallback, useRef } from "react";
 import { useRouter } from "next/router";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
