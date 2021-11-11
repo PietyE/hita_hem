1f421802cf1d22bccac91fdc90d9726dcdad3f09
Mykhailo Kostenko <mykhailo.kostenko@vilmate.com>
2021-11-11 19:09:22 +0200

    added months on swedish

--- containers/ProfilePage/PersonalDetails.jsx
+++ containers/ProfilePage/PersonalDetails.jsx
@@ -29,6 +29,7 @@ import {restrictOnlyLetters, restrictCity,restrictLettersNumbersAndSpecialCharac
 import { getPrivacyPolicyDocument } from "redux/reducers/documents";
 import useProfileErrorHandler from "customHooks/useProfileErrorHandler";
 import {recaptcha} from "../../utils/recaptcha";
+import {getSelectedLangSelector} from "../../redux/reducers/language";
 
 const PersonalDetails = ({
   type,
@@ -42,6 +43,7 @@ const PersonalDetails = ({
   const errorHandlerHook = useProfileErrorHandler();
   const dispatch = useDispatch();
   const profile = useSelector(getProfile, isEqual);
+  const language = useSelector(getSelectedLangSelector)
   let initialValues = {
     address: {
       country: "",
@@ -283,7 +285,7 @@ const PersonalDetails = ({
                           {months.map((el) => {
                             return (
                               <option key={el.id} value={el.id}>
-                                {el.month}
+                                {language === "en" ? el.month : el.monthSw}
                               </option>
                             );
                           })}
