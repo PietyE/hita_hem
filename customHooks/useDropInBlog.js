9fc36b4512bc4dfa6128ff1ae3fbd8a47333f7be
Mykhailo Kostenko <mykhailo.kostenko@vilmate.com>
2021-12-16 19:22:54 +0200

    add blog to the ho\me page

--- customHooks/useDropInBlog.js
+++ customHooks/useDropInBlog.js
@@ -1,22 +1,24 @@
-import React, { useEffect } from 'react';
-import {useSelector} from "react-redux";
-import {getSelectedLangSelector} from "../redux/reducers/language";
+import { useEffect } from 'react';
+// import {useSelector} from "react-redux";
+// import {getSelectedLangSelector} from "../redux/reducers/language";
 const useDropInBlog = () => {
-    const lang = useSelector(getSelectedLangSelector)
-
-    useEffect(()=>{
-        if(lang === 'en'){
-            window.dib_categories = 'english'
-        }
-        if(lang === 'sv'){
-            window.dib_categories = 'swedish'
-        }
-    },[lang])
+    // const lang = useSelector(getSelectedLangSelector)
+    //
+    // useEffect(()=>{
+    //     if(lang === 'en'){
+    //         window.dib_categories = 'english'
+    //     }
+    //     if(lang === 'sv'){
+    //         window.dib_categories = 'swedish'
+    //     }
+    // },[lang])
 
     useEffect(() => {
         const script = document.createElement('script');
-        window.dib_id = process.env.NEXT_PUBLIC_DIP_ID;
-        // window.dib_categories = lang === 'en' ? 'english' : 'swedish';
+        if(typeof window !== 'undefined'){
+            window.dib_id = process.env.NEXT_PUBLIC_DIP_ID;
+            window.dib_specific_posts = '1,2';
+        }
         script.src = "https://io.dropinblog.com/js/embed.js";
         script.async = true;
         document.body.appendChild(script);
