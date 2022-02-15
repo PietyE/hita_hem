f3a068884ce383c39fbc1fa2d7a36a1079b9cde9
Kostenko.m <mykhailo.kostenko@vilmate.com>
2022-02-15 13:11:55 +0200

    fix display change password an email blocks for bankId user after fist login

--- redux/reducers/user.js
+++ redux/reducers/user.js
@@ -97,7 +97,7 @@ export const getCanResetPasswordSelector = (state) => state.user.canResetPasswor
 export const getTokenForQuizSocialsSignIn = (state) => state.user.tokenForQuizSocialsSignIn;
 export const getCurrentPath = (state) => state?.user?.currentPath;
 
-export const getIsBankIdResident = (state) => state?.user?.isBankIdResident;
+export const getIsBankIdResident = (state) => state?.user?.isBankIdResident || state?.user?.account?.is_bank_id_resident;
 export const getIsSocialAccount = (state) => state?.user?.account?.social_accounts;
 
 
