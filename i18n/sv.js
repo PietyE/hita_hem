37d6c2403371315782371488a1bf743d67d4297c
Kostenko.m <mykhailo.kostenko@vilmate.com>
2022-02-17 11:45:29 +0200

    add blog page in two languages

--- i18n/sv.js
+++ i18n/sv.js
@@ -326,7 +326,7 @@ profile_page: {
             amount: "Investerat belopp",
             shares: "Antal aktier",
         button: "Kontakta oss",
-        empty_investments_list_text: "Du har inga kampanjer ännu, om du är intresserad av att resa pengar ",
+        empty_investments_list_text: "Du har inte investerat i några kampanjer ännu, om du är intresserad av att se vilka kampanjer som finns på Accumeo ",
         empty_investments_list_link: "klicka här.",
     },
     campaigns: {
@@ -337,7 +337,7 @@ profile_page: {
             end_date: "Slutdatum",
             status: "Status",
             raised: "Reserverat belopp",
-        empty_campaigns_list_text: "Du har inte investerat i några kampanjer ännu, om du är intresserad av att se vilka kampanjer som finns på Accumeo ",
+        empty_campaigns_list_text: "Du har inga kampanjer ännu, om du är intresserad av att resa pengar ",
         empty_campaigns_list_link: "klicka här.",
     },
     personal: {
