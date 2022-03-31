e3a9a47d7b6ce2aafd83c6a64dbdfcfe165ab4b5
Kostenko.m <mykhailo.kostenko@vilmate.com>
2022-03-31 18:58:22 +0300

    add signUp banner to spinner on home page

---
+++ components/BannerSignUpBlock/BannerSignUpBlock.jsx
@@ -0,0 +1,119 @@
+import React, {useCallback, useRef} from 'react';
+import {GoogleLogin} from "react-google-login";
+import {useDispatch, useSelector} from "react-redux";
+import {useTranslation} from "react-i18next";
+import {makeRequestForSignInWithBankId, saveEmail, signInWithGoogle} from "../../redux/actions/user";
+import ButtonStyled from "../ui/Button";
+import {setShowSignUp} from "../../redux/actions/authPopupWindows";
+import {getAuthSocialAccountErrorSelector} from "../../redux/reducers/errors";
+import useAuthErrorHandler from "../../customHooks/useAuthErrorHandler";
+
+const BannerSignUpBlock = () => {
+    const { t } = useTranslation();
+    const dispatch = useDispatch();
+    const inputRef = useRef(null)
+    const socialAccountError = useSelector(getAuthSocialAccountErrorSelector)
+    const errorHandlerHook = useAuthErrorHandler()
+
+    const _signInWithGoogle = useCallback(
+        (values) => {
+            dispatch(signInWithGoogle(values));
+        },
+        [dispatch]
+    );
+
+    const _signInWithBankId = useCallback(
+        () => {
+            dispatch(makeRequestForSignInWithBankId());
+        },
+        [dispatch]
+    );
+
+    const _setShowSignUp = useCallback(
+        (data) => {
+            dispatch(setShowSignUp(data));
+        },
+        [dispatch]
+    );
+
+    const _saveEmail = useCallback(
+        (data) => {
+            dispatch(saveEmail(data));
+        },
+        [dispatch]
+    );
+
+    const handleSignInWithBankId = (e) => {
+        e.preventDefault()
+        if(socialAccountError){
+            errorHandlerHook._clearErrors()
+        }
+        _signInWithBankId()
+    }
+
+    const responseGoogle = (response) => {
+        if(socialAccountError){
+            errorHandlerHook._clearErrors()
+        }
+        _signInWithGoogle(response.tokenId)
+    }
+
+    const handleSignUp = () => {
+        _setShowSignUp(true);
+        if(inputRef?.current?.value){
+            _saveEmail(inputRef.current.value)
+            inputRef.current.value = ''
+        }
+        if(socialAccountError){
+            errorHandlerHook._clearErrors()
+        }
+    }
+
+    return (
+        <div className='banner_button_wrapper'>
+
+            <label className='banner_label'>
+                <input ref={inputRef} placeholder='Your email address' type='text' className='banner_input'/>
+                <ButtonStyled
+                    colorStyle='dark-green'
+                    className='banner_button'
+                    onClick={handleSignUp}
+                >
+                    Register
+                </ButtonStyled>
+            </label>
+
+            <p className='sign_in_alt_text'>or</p>
+
+            <div className='sign_in_socials_buttons_wrapper'>
+                <button className='sign_in_bank_id sign_in_social_button' onClick={handleSignInWithBankId}>
+                    BankID
+                </button>
+
+                <GoogleLogin
+                    clientId= {process.env.NEXT_PUBLIC_GOOGLE_OAUTH}
+                    // buttonText="Google"
+                    render={renderProps => (
+                        <button
+                            className='sign_in_google sign_in_social_button'
+                            onClick={renderProps.onClick}
+                            disabled={renderProps.disabled}
+                        >
+                            <span>Google</span>
+                        </button>
+                    )}
+                    onSuccess={responseGoogle}
+                    onFailure={responseGoogle}
+                    cookiePolicy={'single_host_origin'}
+                />
+
+            </div>
+            {socialAccountError && (
+                <p className='sign_in_socials_account_error'>{socialAccountError}</p>
+            )}
+        </div>
+
+    );
+}
+
+export default BannerSignUpBlock;
--- components/TopSliderComponent/TopSliderComponent.jsx
+++ components/TopSliderComponent/TopSliderComponent.jsx
@@ -5,7 +5,7 @@ import Button from "../ui/Button";
 import StatusCompanyBadge from "../StatusCompany";
 import Image from 'next/image'
 import {checkCurrentResolution, getCorrectImage} from "../../utils/utils";
-
+import BannerSignUpBlock from "../BannerSignUpBlock";
 const TopSliderComponent = ({
   data,
   sectionClass,
@@ -20,9 +20,69 @@ const TopSliderComponent = ({
 }) => {
   const screenSize = checkCurrentResolution()
 
+  //////////////////////
+  const img = 'https://d190e604gdbcnz.cloudfront.net/Header_29.07_u3ObFQc.png'
+  //////////////////////
+
   return (
     <div className={`slider_component_container ${sectionClass}`}>
-      <Carousel controls={data?.length>1} slide={true} interval={8000} touch={true} indicators={data?.length>1}>
+      <Carousel controls={data?.length} slide={true} interval={8888000} touch={true} indicators={data?.length}>
+        {type === 'home_page' && (
+            <Carousel.Item key = 'banner'>
+              <section className='item_component_container' style={{  position: 'relative'}}>
+                <div className= {`item_component_content_container ${containerClass}`}>
+
+                  <h1 className={`item_component_title ${itemTitleClass}`}>
+                    We democratise unlisted stocks!
+                  </h1>
+
+                      <div className={`item_component_description ${itemDescriptionClass}`}>
+                        <p>
+                          Accumeo allows access to unlisted companies through equity crowdfunding
+                        </p>
+                      </div>
+
+<BannerSignUpBlock/>
+
+                </div>
+
+                {img && (
+                    <div className='item_component_image' style={{  position: 'absolute'}}>
+                      {( screenSize === 'desktop' && img &&
+                          <Image
+                              src = {img || null}
+                              layout = "fill"
+                              objectFit = "cover"
+                              priority = {true}
+                              alt = {img ? 'header image' : ' '}
+
+                          />)}
+                      {( screenSize === 'laptop' && img &&
+                          <Image
+                              src = {img || null}
+                              layout = "fill"
+                              objectFit = "cover"
+                              priority = {true}
+                              alt = {img ? 'header image' : ' '}
+
+                          />)}
+                      {( screenSize === 'mobile' && img &&
+                          <Image
+                              src = {img || null}
+                              layout = "fill"
+                              objectFit = "cover"
+                              priority = {true}
+                              alt = {img ? 'header image' : ' '}
+
+                          />)}
+                    </div>
+                )}
+              </section>
+
+
+
+            </Carousel.Item>
+        )}
         {!!data?.length &&
           data?.map((headerItem) => {
             const {
@@ -41,33 +101,6 @@ const TopSliderComponent = ({
             return (
               <Carousel.Item key={pk+title}>
                 <div className='item_component_container' style={{  position: 'relative'}}>
-                  {/*{( screenSize === 'desktop' && img &&*/}
-                  {/*    <Image*/}
-                  {/*    src = {img}*/}
-                  {/*    layout = "fill"*/}
-                  {/*    objectFit = "cover"*/}
-                  {/*    priority = {true}*/}
-                  {/*    alt = 'header image'*/}
-
-                  {/*/>)}*/}
-                  {/*{( screenSize === 'laptop' && img &&*/}
-                  {/*    <Image*/}
-                  {/*        src = {img}*/}
-                  {/*        layout = "fill"*/}
-                  {/*        objectFit = "cover"*/}
-                  {/*        priority = {true}*/}
-                  {/*        alt = 'header image'*/}
-
-                  {/*    />)}*/}
-                  {/*{( screenSize === 'mobile' && img &&*/}
-                  {/*    <Image*/}
-                  {/*        src = {img}*/}
-                  {/*        layout = "fill"*/}
-                  {/*        objectFit = "cover"*/}
-                  {/*        priority = {true}*/}
-                  {/*        alt = 'header image'*/}
-
-                  {/*    />)}*/}
                   <div className= {`item_component_content_container ${containerClass}`}>
                     {status && (
                         <StatusCompanyBadge
