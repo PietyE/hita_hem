4b8a01161c74b63e98bd4e40dbb21399179b6007
Kostenko.m <mykhailo.kostenko@vilmate.com>
2022-04-01 15:46:11 +0300

    try with dynamic import

--- components/TopSliderComponent/TopSliderComponent.jsx
+++ components/TopSliderComponent/TopSliderComponent.jsx
@@ -1,148 +1,151 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
-import { sanitizeHtmlFromBack } from "utils/sanitazeHTML";
+import {sanitizeHtmlFromBack} from "utils/sanitazeHTML";
 import Button from "../ui/Button";
 import StatusCompanyBadge from "../StatusCompany";
 import {checkCurrentResolution, getCorrectImage} from "../../utils/utils";
-import BannerSignUpBlock from "../BannerSignUpBlock";
 import SliderImage from "../../containers/HomePage/SliderImage";
-import {useSelector} from "react-redux";
-import {getBannerSelector} from "../../redux/reducers/homePage";
+import dynamic from "next/dynamic";
+
+const BannerSignUpBlock = dynamic(() =>
+    import("components/BannerSignUpBlock"), {ssr: false}
+);
+
+
 const TopSliderComponent = ({
-  data,
-  sectionClass,
-  containerClass,
-  statusClass,
-  itemTitleClass,
-  itemDescriptionClass,
-  buttonsContainerClass,
-  firstButtonClass,
-  secondButtonClass,
-    type
-}) => {
-  const screenSize = checkCurrentResolution()
+                                data,
+                                sectionClass,
+                                containerClass,
+                                statusClass,
+                                itemTitleClass,
+                                itemDescriptionClass,
+                                buttonsContainerClass,
+                                firstButtonClass,
+                                secondButtonClass,
+                                type,
+                                bannerData,
+                            }) => {
+    const screenSize = checkCurrentResolution()
 
-  const bannerData = useSelector(getBannerSelector)
+    return (
+        <div className={`slider_component_container ${sectionClass}`}>
+            <Carousel controls={bannerData ? !!data?.length : data?.length > 1} slide={true} interval={8000}
+                      touch={true} indicators={bannerData ? !!data?.length : data?.length > 1}>
+                {bannerData && type === 'home_page' && (
+                    <Carousel.Item key='banner'>
+                        <section className='item_component_container' style={{position: 'relative'}}>
+                            <div className={`item_component_content_container ${containerClass}`}>
+                                {bannerData?.title && (
+                                    <h1 className={`item_component_title ${itemTitleClass}`}>
+                                        {bannerData.title}
+                                    </h1>)
+                                }
 
-  return (
-    <div className={`slider_component_container ${sectionClass}`}>
-      <Carousel controls={bannerData ? !!data?.length : data?.length > 1} slide={true} interval={8000} touch={true} indicators={bannerData ? !!data?.length : data?.length > 1}>
-        {bannerData && (
-            <Carousel.Item key = 'banner'>
-              <section className='item_component_container' style={{  position: 'relative'}}>
-                <div className= {`item_component_content_container ${containerClass}`}>
-                  {bannerData?.title && (
-                        <h1 className={`item_component_title ${itemTitleClass}`}>
-                      {bannerData.title}
-                    </h1>)
-                  }
+
+                                {bannerData?.sub_title && (
+                                    <div className={`item_component_description ${itemDescriptionClass}`}>
+                                        <p>
+                                            {bannerData.sub_title}
+                                        </p>
+                                    </div>
+                                )
+                                }
+
+                                <BannerSignUpBlock/>
+
+                            </div>
+                            {bannerData?.images && screenSize && (
+                                <SliderImage img={getCorrectImage(bannerData.images)} screenSize={screenSize}/>
+                            )}
+
+                        </section>
 
 
-                  {bannerData?.sub_title && (
-                      <div className={`item_component_description ${itemDescriptionClass}`}>
-                        <p>
-                          {bannerData.sub_title}
-                        </p>
-                      </div>
-                  )
-                  }
-
-<BannerSignUpBlock/>
-
-                </div>
-                {bannerData?.images && screenSize && (
-                    <SliderImage img={getCorrectImage(bannerData.images)} screenSize={screenSize}/>
+                    </Carousel.Item>
                 )}
+                {!!data?.length &&
+                data?.map((headerItem) => {
+                    const {
+                        images,
+                        title,
+                        description,
+                        first_button_title,
+                        second_button_title,
+                        first_button_url,
+                        second_button_url,
+                        status,
+                        pk,
+                        percentage,
+                    } = headerItem;
+                    const img = getCorrectImage(images)
+                    return (
+                        <Carousel.Item key={pk + title}>
+                            <div className='item_component_container' style={{position: 'relative'}}>
+                                <div className={`item_component_content_container ${containerClass}`}>
+                                    {status && (
+                                        <StatusCompanyBadge
+                                            status={status}
+                                            percentage={percentage}
+                                            classNameContainer={`item_component_status ${statusClass}`}
+                                        />
+                                    )}
+                                    {title && type === 'home_page' && (
+                                        <h1 className={`item_component_title ${itemTitleClass}`}>
+                                            {title}
+                                        </h1>
+                                    )}
+                                    {title && type !== 'home_page' && (
+                                        <h2 className={`item_component_title ${itemTitleClass}`}>
+                                            {title}
+                                        </h2>
+                                    )}
+                                    {description && (
+                                        <div
+                                            className={`item_component_description ${itemDescriptionClass}`}
+                                            dangerouslySetInnerHTML={{
+                                                __html: sanitizeHtmlFromBack(description),
+                                            }}
+                                        />
+                                    )}
+                                    {(first_button_title || second_button_title) && (
+                                        <div
+                                            className={`slider_component_buttons_container ${buttonsContainerClass}`}
+                                        >
+                                            {first_button_title && (
+                                                <a href={first_button_url}>
+                                                    <Button
+                                                        className={`slider_component_first_btn ${firstButtonClass}`}
+                                                        colorStyle="white"
+                                                    >
+                                                        {first_button_title.toUpperCase()}
+                                                    </Button>
+                                                </a>
+                                            )}
+                                            {second_button_title && (
+                                                <a href={second_button_url}>
+                                                    <Button
+                                                        className={`slider_component_second_btn ${secondButtonClass}`}
+                                                        colorStyle="outline-white"
+                                                    >
+                                                        {second_button_title.toUpperCase()}
+                                                    </Button>
+                                                </a>
+                                            )}
+                                        </div>
+                                    )}
+                                </div>
 
-              </section>
+                                {img && screenSize && (
+                                    <SliderImage img={img} screenSize={screenSize}/>
+                                )}
 
-
-
-            </Carousel.Item>
-        )}
-        {!!data?.length &&
-          data?.map((headerItem) => {
-            const {
-              images,
-              title,
-              description,
-              first_button_title,
-              second_button_title,
-              first_button_url,
-              second_button_url,
-              status,
-                pk,
-                percentage,
-            } = headerItem;
-            const img = getCorrectImage(images)
-            return (
-              <Carousel.Item key={pk+title}>
-                <div className='item_component_container' style={{  position: 'relative'}}>
-                  <div className= {`item_component_content_container ${containerClass}`}>
-                    {status && (
-                        <StatusCompanyBadge
-                            status={status}
-                            percentage={percentage}
-                            classNameContainer={`item_component_status ${statusClass}`}
-                        />
-                    )}
-                    {title && type === 'home_page' && (
-                        <h1 className={`item_component_title ${itemTitleClass}`}>
-                          {title}
-                        </h1>
-                    )}
-                    {title && type !== 'home_page' && (
-                        <h2 className={`item_component_title ${itemTitleClass}`}>
-                          {title}
-                        </h2>
-                    )}
-                    {description && (
-                        <div
-                            className={`item_component_description ${itemDescriptionClass}`}
-                            dangerouslySetInnerHTML={{
-                              __html: sanitizeHtmlFromBack(description),
-                            }}
-                        />
-                    )}
-                    {(first_button_title || second_button_title) && (
-                        <div
-                            className={`slider_component_buttons_container ${buttonsContainerClass}`}
-                        >
-                          {first_button_title && (
-                              <a href={first_button_url}>
-                                <Button
-                                    className={`slider_component_first_btn ${firstButtonClass}`}
-                                    colorStyle="white"
-                                >
-                                  {first_button_title.toUpperCase()}
-                                </Button>
-                              </a>
-                          )}
-                          {second_button_title && (
-                              <a href={second_button_url}>
-                                <Button
-                                    className={`slider_component_second_btn ${secondButtonClass}`}
-                                    colorStyle="outline-white"
-                                >
-                                  {second_button_title.toUpperCase()}
-                                </Button>
-                              </a>
-                          )}
-                        </div>
-                    )}
-                  </div>
-
-                  {img && screenSize && (
-                      <SliderImage img={img} screenSize={screenSize}/>
-                  )}
-
-                </div>
-              </Carousel.Item>
-            );
-          })}
-      </Carousel>
-    </div>
-  );
+                            </div>
+                        </Carousel.Item>
+                    );
+                })}
+            </Carousel>
+        </div>
+    );
 };
 
 export default TopSliderComponent;
--- redux/reducers/homePage.js
+++ redux/reducers/homePage.js
@@ -1,54 +1,54 @@
 import {
-  SET_HOME_PAGE,
-  SET_IS_FETCHING_HOME_PAGE,
+    SET_HOME_PAGE,
+    SET_IS_FETCHING_HOME_PAGE,
 } from "constants/actionsConstant";
 
 const initialState = {
-  homePageContent: {
-    upcoming: [],
-    future: [],
-    headers: [],
-    raise_set: [],
-    invest_set: [],
-    seo:{},
-    banner_images:[],
-  },
-  isFetching: false,
+    homePageContent: {
+        upcoming: [],
+        future: [],
+        headers: [],
+        raise_set: [],
+        invest_set: [],
+        seo: {},
+        banner_images: [],
+    },
+    isFetching: false,
 };
 
 const sortedByIndex = (data) => data.sort((a, b) => a.index - b.index);
 
 export const getHomePageHeadersSelector = (state) =>
-  sortedByIndex(state.homePage.homePageContent.headers);
+    sortedByIndex(state.homePage.homePageContent.headers);
 export const getHomePageFutureSelector = (state) =>
-  sortedByIndex(state.homePage.homePageContent.future);
+    sortedByIndex(state.homePage.homePageContent.future);
 export const getHomePageRaisesSelector = (state) =>
-  sortedByIndex(state.homePage.homePageContent.raise_set);
+    sortedByIndex(state.homePage.homePageContent.raise_set);
 export const getHomePageInvestSelector = (state) =>
-  sortedByIndex(state.homePage.homePageContent.invest_set);
+    sortedByIndex(state.homePage.homePageContent.invest_set);
 export const getHomePageUpcomingSelector = (state) =>
-  sortedByIndex(state.homePage.homePageContent.upcoming).filter(el=>el.status === 1);
+    sortedByIndex(state.homePage.homePageContent.upcoming).filter(el => el.status === 1);
 export const getIsFetchingHomePageSelector = (state) =>
-  state.homePage.isFetching;
+    state.homePage.isFetching;
 export const getSeoSelector = (state) =>
     state.homePage.homePageContent?.seo;
 export const getBannerSelector = (state) => {
-  const data = state.homePage.homePageContent?.banner_images;
-  if(Array.isArray(data) && data.length > 0){
-    return data[0]
-  }else {
-    return null
-  }
+    const data = state.homePage.homePageContent?.banner_images;
+    if (Array.isArray(data) && data.length > 0) {
+        return data[0]
+    } else {
+        return null
+    }
 }
 
 
 export const homePage = (state = initialState, actions) => {
-  switch (actions.type) {
-    case SET_HOME_PAGE:
-      return { ...state, homePageContent: actions.payload };
-    case SET_IS_FETCHING_HOME_PAGE:
-      return { ...state, isFetching: actions.payload };
-    default:
-      return state;
-  }
+    switch (actions.type) {
+        case SET_HOME_PAGE:
+            return {...state, homePageContent: actions.payload};
+        case SET_IS_FETCHING_HOME_PAGE:
+            return {...state, isFetching: actions.payload};
+        default:
+            return state;
+    }
 };
