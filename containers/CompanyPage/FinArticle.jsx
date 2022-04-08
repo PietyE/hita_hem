a270d06ff85c971ec91ef4ab394b9fc0aba5a23e
Kostenko.m <mykhailo.kostenko@vilmate.com>
2022-04-08 21:54:51 +0300

    added nextJs Image component with the right size to the idea tab

--- containers/CompanyPage/FinArticle.jsx
+++ containers/CompanyPage/FinArticle.jsx
@@ -1,28 +1,40 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, {useEffect, useRef, useState} from "react";
 import Button from "components/ui/Button";
-import { useMediaQueries } from "@react-hook/media-query";
-import { sanitizeHtmlFromBack } from "utils/sanitazeHTML";
+import {useMediaQueries} from "@react-hook/media-query";
+import {sanitizeHtmlFromBack} from "utils/sanitazeHTML";
 import {useTranslation} from "react-i18next";
-import {getCorrectImage} from "../../utils/utils";
+import {getCorrectImage, getImgMeta} from "../../utils/utils";
 import Image from "next/image";
-import {getImgMeta} from "../../utils/utils";
 import dynamic from "next/dynamic";
-// import ImageComponent from "../../components/ui/ImageComponent";
 
 const SimpleReactLightbox = dynamic(() => import("simple-react-lightbox"), {
     ssr: false,
 });
 
 const SRLWrapper = dynamic(() =>
-    import('simple-react-lightbox').then((mod) => mod.SRLWrapper),{ssr: false}
+    import('simple-react-lightbox').then((mod) => mod.SRLWrapper), {ssr: false}
 )
 
+const options = {
+    buttons: {
+        showDownloadButton: false,
+        showAutoplayButton: false,
+        showNextButton: false,
+        showPrevButton: false,
+        showThumbnailsButton: false,
+        showFullscreenButton: false,
 
-const FinArticle = ({ item }) => {
-  const { t } = useTranslation();
+    },
+    thumbnails: {
+        showThumbnails: false,
+    }
+}
 
-  const { title, description, images } = item;
-  const img = getCorrectImage(images)
+const FinArticle = ({item}) => {
+    const {t} = useTranslation();
+
+    const {title, description, images} = item;
+    const img = getCorrectImage(images)
 
     const contentRef = useRef();
 
@@ -31,14 +43,14 @@ const FinArticle = ({ item }) => {
     const [itemClass, setItemClass] = useState('fin_article')
     const [imageMeta, setImageMeta] = useState({})
 
-  const { matchesAll } = useMediaQueries({
-    screen: "screen",
-    width: "(max-device-width: 900px)",
-  });
+    const {matchesAll} = useMediaQueries({
+        screen: "screen",
+        width: "(max-device-width: 900px)",
+    });
 
-    useEffect(()=>{
+    useEffect(() => {
         getImgMeta(img, setImageMeta)
-    },[])
+    }, [])
 
     useEffect(() => {
         if (!isShowButton) {
@@ -88,84 +100,60 @@ const FinArticle = ({ item }) => {
             window.scrollTo({top: scrolled - 160})
         }
     };
-  const options = {
-    buttons: {
-      showDownloadButton:false,
-      showAutoplayButton:false,
-      showNextButton:false,
-      showPrevButton:false,
-      showThumbnailsButton:false,
-      showFullscreenButton:false,
-
-    },
-    thumbnails: {
-      showThumbnails: false,
-    }
-  }
 
     return (
         <section className="fin_article_wrapper">
-          <SimpleReactLightbox>
-            <SRLWrapper options={options}>
-            <li className={itemClass}
-                ref={contentRef}
-            >
-                <div
-                    className={
-                        img
-                            ? "fin_article_text_wrapper"
-                            : "fin_article_text_wrapper_alone"
-                    }
-                >
-                    <h3 className="fin_article_title">{title}</h3>
-                    <span
-                        className="fin_article_text"
-                        dangerouslySetInnerHTML={{
-                            __html: sanitizeHtmlFromBack(description),
-                        }}
-                    />
-                </div>
-                {!!img && (
-                    // <ImageComponent
-                    //     className="fin_article_image"
-                    //     src={img}
-                    //     alt={img ? 'article image' : ' '}
-                    // />
-                    //   <div className='fin_article_image' style={{  position: 'relative'}}>
-                    //
-                      <Image
-                              src = {img}
-                              layout = "responsive"
-                              width={imageMeta?.width ||192}
-                              height={imageMeta?.height || 108}
-                              className='fin_article_image'
-                              // priority = {true}
-
-                          />
-                    //   </div>
-                )}
-                {matchesAll && isShowButton && (
-                    <div className={isShowMore ? "show_more show_more_clicked" : "show_more "}>
+            <SimpleReactLightbox>
+                <SRLWrapper options={options}>
+                    <li className={itemClass}
+                        ref={contentRef}
+                    >
+                        <div
+                            className={
+                                img
+                                    ? "fin_article_text_wrapper"
+                                    : "fin_article_text_wrapper_alone"
+                            }
+                        >
+                            <h3 className="fin_article_title">{title}</h3>
+                            <span
+                                className="fin_article_text"
+                                dangerouslySetInnerHTML={{
+                                    __html: sanitizeHtmlFromBack(description),
+                                }}
+                            />
+                        </div>
+                        {!!img && (
+                            <Image
+                                src={img}
+                                layout="responsive"
+                                width={imageMeta?.width || 192}
+                                height={imageMeta?.height || 108}
+                                className='fin_article_image'
+                            />
+                        )}
+                        {matchesAll && isShowButton && (
+                            <div className={isShowMore ? "show_more show_more_clicked" : "show_more "}>
             <span
                 className="show_more_button fin_show_more"
                 onClick={_handleClickShowMore}
             >
               {isShowMore ? t("company_page.button_show_less") : t("company_page.button_show_more")}
             </span>
-                    </div>
-                )}
-                {!matchesAll && isShowButton && (
-                    <Button
-                        onClick={_handleClickShowMore}
-                        colorStyle="red-without-border"
-                        className={isShowMore ? "fin_article_large_button " : 'fin_article_large_button button_clicked'}
-                    >
-                        {isShowMore ? t("company_page.button_show_less") : t("company_page.button_read_more")}
-                    </Button>
-                )}
-            </li>
-            </SRLWrapper>
-          </SimpleReactLightbox>
+                            </div>
+                        )}
+                        {!matchesAll && isShowButton && (
+                            <Button
+                                onClick={_handleClickShowMore}
+                                colorStyle="red-without-border"
+                                className={isShowMore ? "fin_article_large_button " : 'fin_article_large_button button_clicked'}
+                            >
+                                {isShowMore ? t("company_page.button_show_less") : t("company_page.button_read_more")}
+                            </Button>
+                        )}
+                    </li>
+                </SRLWrapper>
+            </SimpleReactLightbox>
         </section>
     );
 };
