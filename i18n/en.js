153fef1036b65ed3dea1a0a38dd742b4380ba229
Kostenko.m <mykhailo.kostenko@vilmate.com>
2022-04-14 11:14:36 +0300

    fix dot in email example en

--- i18n/en.js
+++ i18n/en.js
@@ -419,7 +419,7 @@ export const en = {
             },
         },
         errors: {
-            email_example: 'Example: test@test.com.',
+            email_example: 'Example: test@test.com',
             email_required: 'Email is a required field.',
             password_required: 'Password is a required field.',
             password_example: 'The password must contain at least eight characters, at least one capital letter and at least one number.',
