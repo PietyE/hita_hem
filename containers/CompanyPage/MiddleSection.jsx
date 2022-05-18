eb4147ab4e1e575c00351f194596aa2360e9caad
Serhij Zubrin <absminoga@outlook.com>
2022-05-18 08:14:39 +0300

    Fixed a bug with displaying the content of the sections Idea Team Finfo

--- containers/CompanyPage/MiddleSection.jsx
+++ containers/CompanyPage/MiddleSection.jsx
@@ -1,6 +1,7 @@
 import React, {useCallback, useEffect, useRef, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import dynamic from "next/dynamic";
+import {useRouter} from "next/router";
 
 import Overview from "./Overview";
 import TabAccordion from "components/ui/TabAccordion";
@@ -23,7 +24,11 @@ const CampaignTabSignUp = dynamic(() =>
 
 
 import {companyTabConstants} from "constants/companyTabConstant";
-import {setSelectedTab} from "redux/actions/companies";
+import {
+    setSelectedTab,
+    getCompanyBySlag,
+    clearCompany,
+} from "redux/actions/companies";
 import {
     getCompanyIndustryTitleSelector, getCompanyLogoUrlSelector, getCompanyNameSelector,
     getCompanyTabSelected,
@@ -44,6 +49,7 @@ import throttle  from "lodash/throttle"
 const MiddleSection = ({isAuth}) => {
     const {t} = useTranslation();
     const dispatch = useDispatch();
+    const router = useRouter();
     const selectedTab = useSelector(getCompanyTabSelected);
     const isQuizPassed = useSelector(getQuizIsPassedSelector)
     const industryTitle = useSelector(getCompanyIndustryTitleSelector);
@@ -56,6 +62,8 @@ const MiddleSection = ({isAuth}) => {
     const alter_text = useSelector(getCompanyLogoAltTextSelector)
     const sectionRef = useRef();
     const sectionsContainerRef = useRef()
+   
+    const companyName = router?.query?.companyId;
 
     const _changeCompanuTab = useCallback(
         (key) => {
@@ -64,6 +72,16 @@ const MiddleSection = ({isAuth}) => {
         [dispatch]
     );
 
+    const _getCompanyDetail = useCallback(
+        (name) => {
+            dispatch(getCompanyBySlag(name));
+        },
+        [dispatch]
+    );
+
+    const _clearCompanyDetail = useCallback(() => {
+        dispatch(clearCompany());
+    }, [dispatch]);
 
     const {matchesAll} = useMediaQueries({
         screen: "screen",
@@ -192,6 +210,15 @@ const MiddleSection = ({isAuth}) => {
             window.removeEventListener("scroll", sectionsTracking)
         }
     },[showFaq, isAuth, isQuizPassed])
+
+    useEffect(() => {
+        if (isQuizPassed) {
+            _getCompanyDetail(companyName)
+        }
+        return () => {
+            _clearCompanyDetail();
+        };
+    }, [isQuizPassed])
 
 
 
