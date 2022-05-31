c35ec5ef9f4d4cffc385eab170e9f401eba47e3f
Kostenko.m <mykhailo.kostenko@vilmate.com>
2022-05-31 10:15:55 +0300

    changed the font weight in the change account type popup

--- components/SocialsAuthButtons/SocialsAuthButtons.jsx
+++ components/SocialsAuthButtons/SocialsAuthButtons.jsx
@@ -19,28 +19,26 @@ const SocialsAuthButtons = ({containerClassName, type}) => {
 
 
     const responseGoogle = (response) => {
-        if(socialAccountError){
+        if (socialAccountError) {
             errorHandlerHook._clearErrors()
         }
 
-        if(type === 'change_account_type' ){
-            // _setShowCompleteChangeAccountType('Google')
-            recaptcha('change_account_type', _changeAccountType, {token:response.tokenId, provider: 'google'})
-        }else{
+        if (type === 'change_account_type') {
+            recaptcha('change_account_type', _changeAccountType, {token: response.tokenId, provider: 'google'})
+        } else {
             _signInWithGoogle(response.tokenId)
         }
     }
 
     const handleSignInWithBankId = (e) => {
         e.preventDefault()
-        if(socialAccountError){
+        if (socialAccountError) {
             errorHandlerHook._clearErrors()
         }
-        if(type === 'change_account_type' ){
+        if (type === 'change_account_type') {
             _setChangeableAccountType('BankID')
             _signInWithBankId()
-            // recaptcha('change_account_type', _changeAccountType, {token:response.tokenId, provider: 'bankid'})
-        }else {
+        } else {
             _signInWithBankId()
         }
     }
@@ -75,13 +73,13 @@ const SocialsAuthButtons = ({containerClassName, type}) => {
         <>
             <div className={`${containerClassName} socials_buttons_wrapper`}>
                 <button className='auth_bank_id auth_social_button'
-                    onClick={handleSignInWithBankId}
+                        onClick={handleSignInWithBankId}
                 >
                     <span>BankID</span>
                 </button>
 
                 <GoogleLogin
-                    clientId= {process.env.NEXT_PUBLIC_GOOGLE_OAUTH}
+                    clientId={process.env.NEXT_PUBLIC_GOOGLE_OAUTH}
                     render={renderProps => (
                         <button
                             className='auth_google auth_social_button'
@@ -100,7 +98,7 @@ const SocialsAuthButtons = ({containerClassName, type}) => {
             {socialAccountError && (
                 <p className='socials_account_error'>{socialAccountError}</p>
             )}
-            </>
+        </>
     );
 }
 
