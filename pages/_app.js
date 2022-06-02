edb739bc84c95bdfc96ca1c443b1cc965f639451
Kostenko.m <mykhailo.kostenko@vilmate.com>
2022-06-02 16:12:38 +0300

    add defer to scripts

--- pages/_app.js
+++ pages/_app.js
@@ -17,38 +17,21 @@ function App({Component, pageProps}) {
                 <title>Accumeo - Investera i onoterade tillväxtbolag idag</title>
                 <meta name = "description"
                       content = "Accumeo gör delägarskap i onoterade bolag åtkomligt för fler genom gräsrotsfinansiering"/>
-
-            </Head>
-
-          {/*  /!* Global Site Tag (gtag.js) - Google Analytics *!/*/}
-          {/*  <Script*/}
-          {/*      strategy="afterInteractive"*/}
-          {/*      dangerouslySetInnerHTML={{*/}
-          {/*          __html: `*/}
-          {/*  window.dataLayer = window.dataLayer || [];*/}
-          {/*  function gtag(){dataLayer.push(arguments);}*/}
-          {/*  gtag('js', new Date());*/}
-          {/*  gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {*/}
-          {/*    page_path: window.location.pathname,*/}
-          {/*  });*/}
-          {/*`,*/}
-          {/*      }}*/}
-          {/*  />*/}
-          {/*  <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}*/}
-          {/*          strategy="afterInteractive"/>*/}
-
-            {/*Intercom*/}
-            <Script strategy = "lazyOnload" id = 'intercom_settings'>
-                {` window.intercomSettings = {
+                <script
+                    // strategy = "lazyOnload"
+                    defer
+                    id = 'intercom_settings'>
+                    {` window.intercomSettings = {
     app_id: "${process.env.NEXT_PUBLIC_INTERCOM_APP_ID}",
     language_override: '${initLang}' || 'sv',
   };`}
-            </Script>
-            <Script
-                id = 'intercom_boot'
-                strategy = "lazyOnload"
-                 dangerouslySetInnerHTML = {{
-                     __html: `
+                </script>
+                <script
+                    id = 'intercom_boot'
+                    // strategy = "lazyOnload"
+                    defer
+                    dangerouslySetInnerHTML = {{
+                        __html: `
 
  (function(){const w=window;
  const ic=w.Intercom;
@@ -79,8 +62,71 @@ function App({Component, pageProps}) {
  }}})();
 
    `,
-                 }}
-            />
+                    }}
+                />
+            </Head>
+
+          {/*  /!* Global Site Tag (gtag.js) - Google Analytics *!/*/}
+          {/*  <Script*/}
+          {/*      strategy="afterInteractive"*/}
+          {/*      dangerouslySetInnerHTML={{*/}
+          {/*          __html: `*/}
+          {/*  window.dataLayer = window.dataLayer || [];*/}
+          {/*  function gtag(){dataLayer.push(arguments);}*/}
+          {/*  gtag('js', new Date());*/}
+          {/*  gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {*/}
+          {/*    page_path: window.location.pathname,*/}
+          {/*  });*/}
+          {/*`,*/}
+          {/*      }}*/}
+          {/*  />*/}
+          {/*  <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}*/}
+          {/*          strategy="afterInteractive"/>*/}
+
+            {/*Intercom*/}
+ {/*           <Script strategy = "lazyOnload" id = 'intercom_settings'>*/}
+ {/*               {` window.intercomSettings = {*/}
+ {/*   app_id: "${process.env.NEXT_PUBLIC_INTERCOM_APP_ID}",*/}
+ {/*   language_override: '${initLang}' || 'sv',*/}
+ {/* };`}*/}
+ {/*           </Script>*/}
+ {/*           <Script*/}
+ {/*               id = 'intercom_boot'*/}
+ {/*               strategy = "lazyOnload"*/}
+ {/*                dangerouslySetInnerHTML = {{*/}
+ {/*                    __html: `*/}
+
+ {/*(function(){const w=window;*/}
+ {/*const ic=w.Intercom;*/}
+ {/*if(typeof ic==="function"){*/}
+ {/*ic('reattach_activator');*/}
+ {/*ic('update',w.intercomSettings);*/}
+ {/*}else{const d=document;*/}
+ {/*const i=function(){i.c(arguments);*/}
+ {/*};*/}
+ {/*i.q=[];*/}
+ {/*i.c=function(args){i.q.push(args);*/}
+ {/*};*/}
+ {/*w.Intercom=i;*/}
+ {/*const l=function(){*/}
+ {/*                setTimeout(function () {*/}
+ {/*                const s=d.createElement('script');*/}
+ {/*s.type='text/javascript';*/}
+ {/*s.async=true;*/}
+ {/*s.src='https://widget.intercom.io/widget/${process.env.NEXT_PUBLIC_INTERCOM_APP_ID}';*/}
+ {/*const x=d.getElementsByTagName('script')[0];*/}
+ {/*x.parentNode.insertBefore(s,x);*/}
+ {/*}, 3000);*/}
+ 
+ {/*};*/}
+ {/*if(document.readyState==='complete'){l();*/}
+ {/*}else if(w.attachEvent){w.attachEvent('onload',l);*/}
+ {/*}else{w.addEventListener('load',l,false);*/}
+ {/*}}})();*/}
+
+ {/*  `,*/}
+ {/*                }}*/}
+ {/*           />*/}
 
             <RootPage initLang = {initLang}>
                 <Component {...pageProps} />
