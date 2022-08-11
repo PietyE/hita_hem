6014f41e021e9e9a35c619e98afac04cefd9a69f
Kostenko.m <mykhailo.kostenko@vilmate.com>
2022-08-11 12:29:25 +0300

    change font to Nunito Sans

--- pages/_document.js
+++ pages/_document.js
@@ -25,15 +25,20 @@ class MyDocument extends Document {
             sizes="16x16"
             href="/favicon-16x16.ico"
           />
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
 
-            <link
-                href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;500;600;700;800;900&display=swap"
-                rel="stylesheet"
-            />
-            <link
-                href="https://fonts.googleapis.com/css2?family=Quicksand:wght@700&display=swap"
-                rel="stylesheet"
-            />
+            <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&family=Nunito+Sans:ital,wght@0,400;0,700;1,400&display=swap"
+                  rel="stylesheet"/>
+            {/*<link*/}
+            {/*    href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;500;600;700;800;900&display=swap"*/}
+            {/*    rel="stylesheet"*/}
+            {/*/>*/}
+            {/*<link*/}
+            {/*    href="https://fonts.googleapis.com/css2?family=Quicksand:wght@700&display=swap"*/}
+            {/*    rel="stylesheet"*/}
+            {/*/>*/}
+
             {/* Global Site Tag (gtag.js) - Google Analytics */}
             <script
                 defer
